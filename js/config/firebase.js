baaca59a41a78e39627016061d741c511af6ffd4
ShontraePasley <shontraep@gmail.com>
2019-11-12 19:56:23 -0600

    changes

---
+++ js/config/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/database";
+import "firebase/auth";
+import "firebase/storage";
+
+firebase.initializeApp(firebaseConfig);
+firebase.firstone();
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCl-504mp4moA-jNbznfHI_MlrkOjOZsss",
+    authDomain: "message-maker-ea728.firebaseapp.com",
+    databaseURL: "https://message-maker-ea728.firebaseio.com",
+    projectId: "message-maker-ea728",
+    storageBucket: "message-maker-ea728.appspot.com",
+    messagingSenderId: "1050658907728",
+    appId: "1:1050658907728:web:55dc2f7b2b14531ff51355",
+    measurementId: "G-VG2CXW5YKY"
+  };
+  
+export default firebase;
+
--- js/index.js
+++ js/index.js
@@ -1,1 +1,2 @@
-
+const main = document.querySelector('.main');
+main.innerText = "Hello World!";
